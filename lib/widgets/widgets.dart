7bf9a7948ba000d21801a758cbf690a247a1523b
Antonio <antonio241710@gmail.com>
2021-09-28 18:49:30 +0200

    added Pantalla de edición de producto

--- lib/widgets/widgets.dart
+++ lib/widgets/widgets.dart
@@ -1,3 +1,4 @@
 export 'package:productos_app/widgets/auth_background.dart';
 export 'package:productos_app/widgets/card_contanier.dart';
 export 'package:productos_app/widgets/product_card.dart';
+export 'package:productos_app/widgets/product_image.dart';
