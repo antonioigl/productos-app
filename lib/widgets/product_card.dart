5ca847e0db045e8a2c419b4cae7c31436455ba6d
Antonio <antonio241710@gmail.com>
2021-10-02 16:52:01 +0200

    added Manejo de imágenes nulas

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -166,12 +166,16 @@ class _BackgroundImage extends StatelessWidget {
       child: Container(
         width: double.infinity,
         height: 400,
-        child: FadeInImage(
-          //TODO: fix productos cuando no hay imagen
-          placeholder: AssetImage('assets/jar-loading.gif'),
-          image: NetworkImage(url!),
-          fit: BoxFit.cover,
-        ),
+        child: url == null
+            ? Image(
+                image: AssetImage('assets/no-image.png'),
+                fit: BoxFit.cover,
+              )
+            : FadeInImage(
+                placeholder: AssetImage('assets/jar-loading.gif'),
+                image: NetworkImage(url!),
+                fit: BoxFit.cover,
+              ),
       ),
     );
   }
