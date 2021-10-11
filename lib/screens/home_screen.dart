2937c69f84d1599ea098a5056cb810d5b885790e
Antonio <antonio241710@gmail.com>
2021-10-11 18:55:31 +0200

    added Cerrar sesión - Logout

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -13,12 +13,20 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
 
     final productsService = Provider.of<ProductsService>(context);
+    final authService = Provider.of<AuthService>(context, listen: false);
 
     if(productsService.isLoading) return LoadingScreen();
 
     return Scaffold(
       appBar: AppBar(
         title: Text('Productos'),
+        leading: IconButton(
+            onPressed: () {
+              authService.logout();
+              Navigator.pushReplacementNamed(context, 'login');
+            },
+            icon: Icon(Icons.login_outlined)
+        ),
       ),
       body: ListView.builder(
           itemCount: productsService.products.length,
